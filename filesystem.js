12f956be1922ba11924367787d81bb52aaeba4a8
Kerry Wang <kerrywang369@gmail.com>
2023-07-08 15:47:45 -0600

    more modular

--- filesystem.js
+++ filesystem.js
@@ -1,5 +1,5 @@
-import * as crypto from "node:crypto";
-import * as fs from "node:fs";
+import crypto from "node:crypto";
+import fs from "node:fs";
 
 export function encryptString(path, plainText, password) {
   const iv = Buffer.alloc(16, 0);
