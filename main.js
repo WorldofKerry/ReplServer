9a2f0d4c7aeb77bda00f726fbbd5b38eaba793c2
Kerry Wang <kerrywang369@gmail.com>
2023-07-07 23:36:38 -0600

    tests

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 import * as fs from "node:fs";
 import * as crypto from "node:crypto";
 
-function encryptString(plainText, password) {
+export function encryptString(path, plainText, password) {
   const iv = Buffer.alloc(16, 0);
   const key = crypto.scryptSync(password, "salt", 24);
   const cipher = crypto.createCipheriv("aes-192-cbc", key, iv);
   let encrypted = cipher.update(plainText, "utf8", "hex");
   encrypted += cipher.final("hex");
 
-  const writeStream = fs.createWriteStream("encrypted.txt");
+  const writeStream = fs.createWriteStream(path);
 
   writeStream.write(encrypted, "utf8");
   writeStream.end();
 
   writeStream.on("finish", () => {
-    console.log("Encryption completed and saved to encrypted.txt");
+    console.log(`Encryption completed and saved to ${path}`);
   });
 }
 
-function decryptString(path, password) {
+export function decryptString(path, password) {
   const iv = Buffer.alloc(16, 0);
   const key = crypto.scryptSync(password, "salt", 24);
   const decipher = crypto.createDecipheriv("aes-192-cbc", key, iv);
@@ -33,15 +33,4 @@ function decryptString(path, password) {
   decipher.write(fileRead, "hex");
   decipher.end();
   return decrypted;
-}
-
-const plainText = "Hello, World!";
-const password = "password";
-
-encryptString(plainText, password);
-
-try {
-  console.log("Decrypted: " + decryptString("encrypted.txt", password));
-} catch (e) {
-  console.log(e);
 }
---
+++ tests.js
@@ -0,0 +1,22 @@
+import * as main from "./main.js";
+import * as assert from "node:assert";
+
+function fileSystemTest() {
+  const plainText = "Hello, World!";
+  const password = "password";
+  const path = "__testData.txt";
+
+  main.encryptString(path, plainText, password);
+
+  setTimeout(() => {
+    try {
+      const decrypted = main.decryptString(path, password);
+      assert.strict(decrypted === plainText);
+      console.log("Decrypted: " + decrypted);
+    } catch (e) {
+      console.log(e);
+    }
+  }, 1000);
+}
+
+fileSystemTest();
