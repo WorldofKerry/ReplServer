030a301b44151f562d51751178faec3887d6d11d
Kerry Wang <kerrywang369@gmail.com>
2023-07-09 18:49:39 -0600

    update password

--- src/envVars.js
+++ src/envVars.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 function getEnvWithDefaults(key) {
-  return process.env[key] === undefined ? "pass" : process.env[key];
+  return process.env[key] === undefined ? "DEFAULT_PASSWORD" : process.env[key];
 }
 
 const ENV_VARS = {
